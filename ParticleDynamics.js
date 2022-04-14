8e800305443286586629ce8365a13adab657765d
Arkdirfe <g.gotschier@gmail.com>
2022-04-14 12:24:43 +0200

    Moved initialization logic to game.js, created simulation timer, moved appropriate data structures to particle dynamics file.

--- ParticleDynamics.js
+++ ParticleDynamics.js
@@ -1,8 +1,21 @@
 ParticleDynamics = {};
 ParticleDynamics.Forces = [];
+ParticleDynamics.DebugLines = [];
 ParticleDynamics.h = 1 / 180;
 ParticleDynamics.eulerSolver = true;
+ParticleDynamics.debugLinesEnabled = false;
+ParticleDynamics.debugLinesLengthMult = 1;
+
+ParticleDynamics.Init = Init;
+ParticleDynamics.UpdateDebug = UpdateDebug;
+ParticleDynamics.EnableForceLines = EnableForceLines;
+ParticleDynamics.DisableForceLines = DisableForceLines;
 ParticleDynamics.UpdateParticle = UpdateParticle;
+
+function Init()
+{
+    SetupDebug();
+}
 
 function UpdateParticle(particle, delta) {
     if (ParticleDynamics.eulerSolver) {
@@ -76,4 +89,160 @@ function CalculateForce(particle) {
     result.velY = particle.force.y;
 
     return result;
+}
+
+////////
+// Debug
+
+function SetupDebug()
+{
+    let xGrid = 50;
+    let yGrid = 50;
+    for(let x = xGrid; x < Game.width; x += xGrid)
+    {
+        for(let y = yGrid; y < Game.height; y += yGrid)
+        {
+            let line = new FieldLine(x, y);
+            ParticleDynamics.DebugLines.push(line);
+            //app.stage.addChild(line);
+        }
+    }//*/
+
+    /*let line = new FieldLine(200, 200);
+    lines.push(line);
+    app.stage.addChild(line);//*/
+}
+
+function EnableForceLines()
+{
+    ParticleDynamics.DebugLines.forEach((line) => {
+        Game.PIXIApp.stage.addChild(line);
+    })
+    ParticleDynamics.debugLinesEnabled = true;
+}
+
+function DisableForceLines()
+{
+    ParticleDynamics.DebugLines.forEach((line) => {
+        Game.PIXIApp.stage.removeChild(line);
+    })
+    ParticleDynamics.debugLinesEnabled = false;
+}
+
+function UpdateDebug(delta)
+{
+    if(!ParticleDynamics.debugLinesEnabled)
+    {
+        return;
+    }
+
+    ParticleDynamics.DebugLines.forEach((line) => {
+        line.update(delta);
+    })
+}
+
+//////////
+// Classes
+
+class Particle {
+    constructor(posX, posY, mass) {
+        this.pos = new Vector2(posX, posY);
+        this.vel = new Vector2(0, 0);
+        this.force = new Vector2(0, 0);
+        this.mass = mass;
+        // For field lines only
+        this.lastFrameForce = new Vector2(0, 0);
+    }
+
+    clearForce()
+    {
+        this.force.x = 0;
+        this.force.y = 0;
+    }
+
+    applyChanges(changes, multiplier)
+    {
+        this.pos.x += changes.posX * multiplier;
+        this.pos.y += changes.posY * multiplier;
+        this.vel.x += changes.velX * multiplier;
+        this.vel.y += changes.velY * multiplier;
+    }
+
+    clone()
+    {
+        let c = new Particle(this.pos.x, this.pos.y, this.mass);
+        c.vel.x = this.vel.x;
+        c.vel.y = this.vel.y;
+        c.force.x = this.force.x;
+        c.force.y = this.force.y;
+        return c;
+    }
+}
+
+class FieldLine extends PIXI.Graphics {
+    constructor(x, y) {
+        super()
+        this.posX = x;
+        this.posY = y;
+        this.particle = new Particle(x, y, 1);
+        this.lineStyle({width: 3, color: 0x000000});
+        this.moveTo(x, y);
+        this.lineTo(x + 1, y + 1);
+    }
+
+    update(delta)
+    {
+        this.particle.pos.x = this.posX;
+        this.particle.pos.y = this.posY;
+        this.particle.vel.x = 0;
+        this.particle.vel.y = 0;
+        ParticleDynamics.UpdateParticle(this.particle, delta);
+        this.clear();
+        this.lineStyle({width: 3, color: 0x000000});
+        this.moveTo(this.posX, this.posY);
+        this.lineTo(this.posX + this.particle.vel.x * ParticleDynamics.debugLinesLengthMult / delta, this.posY + this.particle.vel.y * ParticleDynamics.debugLinesLengthMult / delta);
+    }
+}
+
+class ConstantForce {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    apply(particle)
+    {
+        particle.force.x += this.x / particle.mass;
+        particle.force.y += this.y / particle.mass;
+    }
+}
+
+class RadialForce {
+    constructor(x, y, power, size) {
+        this.center = new Vector2(x, y);
+        this.power = power;
+        this.size = size;
+    }
+
+    apply(particle)
+    {
+        let dir = this.center.subtractPure(particle.pos);
+        let distance = dir.magnitude();
+        dir.normalize();
+        let force = Math.min(this.power / Math.pow(distance / this.size, 2), this.power);
+        particle.force.x += dir.x * force / particle.mass;
+        particle.force.y += dir.y * force / particle.mass;
+    }
+}
+
+class DragForce {
+    constructor(coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    apply(particle)
+    {
+        particle.force.x -= particle.vel.x * this.coefficient;
+        particle.force.y -= particle.vel.y * this.coefficient;
+    }
 }
---
+++ game.js
@@ -0,0 +1,226 @@
+Game = {}
+Game.PIXIApp = undefined;
+Game.width = 800;
+Game.height = 800;
+Game.player = undefined;
+Game.playerSpeed = 5;
+Game.simulationTPS = 60;
+Game.simulationInterval = undefined;
+Game.lastTimestamp = -1;
+Game.useStableDeltas = false; // If false uses exact high-res clock to calculate deltas, if true uses 1/TPS. Eliminates debug line flickering if enabled.
+
+Game.Inputs = {}
+Game.Inputs.Left = undefined;
+Game.Inputs.Right = undefined;
+Game.Inputs.Up = undefined;
+Game.Inputs.Down = undefined;
+
+// Temporary for Particle Dynamics testing
+Game.Forces = [];
+Game.Motion = [];
+
+Game.Init = Init;
+Game.SetSimulationTPS = SetSimulationTPS;
+
+function Init()
+{
+    LoadAssets();
+    ParticleDynamics.Init();
+}
+
+function LoadAssets()
+{
+    const loader = PIXI.Loader.shared;
+    Game.PIXIApp = new PIXI.Application({width: Game.width, height: Game.height, backgroundColor: 0x1099bb});
+    document.body.appendChild(Game.PIXIApp.view); // TODO: Once window decoration is here, attach in a way that fits
+
+    let sprites = {};
+    loader.add("player", "assets/bunny.png");
+    loader.load((loader, resources) => {
+        Game.player = new PIXI.Sprite(resources.player.texture); // We might want to change instantiation to something more dynamic later if we want to have a title screen
+    });
+    loader.onComplete.add(Setup);
+}
+
+function Setup()
+{
+    let player = Game.player;
+    player.position.set(100, 100);
+    player.anchor.set(0.5, 0.5);
+    player.vx = 0;
+    player.vy = 0;
+    Game.PIXIApp.stage.addChild(player);
+
+    // https://github.com/kittykatattack/learningPixi#introduction
+    Game.Inputs.Left = SetupKey("ArrowLeft");
+    Game.Inputs.Up = SetupKey("ArrowUp");
+    Game.Inputs.Right = SetupKey("ArrowRight");
+    Game.Inputs.Down = SetupKey("ArrowDown");
+
+    Game.Inputs.Left.press = () => {
+        player.vx = -Game.playerSpeed;
+        player.vy = 0;
+    };
+    Game.Inputs.Left.release = () => {
+        if(!Game.Inputs.Right.isDown && player.vy === 0) {
+            player.vx = 0;
+        }
+    };
+    Game.Inputs.Up.press = () => {
+        player.vy = -Game.playerSpeed;
+        player.vx = 0;
+    };
+    Game.Inputs.Up.release = () => {
+        if(!Game.Inputs.Down.isDown && player.vx === 0) {
+            player.vy = 0;
+        }
+    };
+    Game.Inputs.Right.press = () => {
+        player.vx = Game.playerSpeed;
+        player.vy = 0;
+    };
+    Game.Inputs.Right.release = () => {
+        if(!Game.Inputs.Left.isDown && player.vy === 0) {
+            player.vx = 0;
+        }
+    };
+    Game.Inputs.Down.press = () => {
+        player.vy = Game.playerSpeed;
+        player.vx = 0;
+    };
+    Game.Inputs.Down.release = () => {
+        if(!Game.Inputs.Up.isDown && player.vx === 0) {
+            player.vy = 0;
+        }
+    };
+
+    Game.PIXIApp.ticker.minFPS = 0;
+    Game.PIXIApp.ticker.maxFPS = 60; // Hardcoded for now, move to UI controls
+
+    const power = 20;
+    const size = 100;
+    const amount = 5;
+    const vel = 75;
+
+    for(let i = 0; i < amount; i++)
+    {
+        let force = new RadialForce(Math.random() * Game.width, Math.random() * Game.height, power, size);
+        ParticleDynamics.Forces.push(force);
+        Game.Forces.push(force);
+        Game.Motion.push(new Vector2(Math.random() * vel - vel / 2, Math.random() * vel - vel / 2));
+    }
+
+    ParticleDynamics.Forces.push(new DragForce(0.1));
+
+    ParticleDynamics.EnableForceLines(); // TODO: Add UI controls
+    Game.SetSimulationTPS(Game.simulationTPS);
+}
+
+function Tick()
+{
+    let delta;
+    if(Game.useStableDeltas || Game.lastTimestamp < 0)
+    {
+        delta = 1 / Game.simulationTPS;
+    }
+    else
+    {
+        delta = (window.performance.now() - Game.lastTimestamp) * 0.001;
+    }
+    Game.lastTimestamp = window.performance.now();
+
+    InterfaceUpdate(delta);
+    SimulationUpdate(delta);
+}
+
+function SimulationUpdate(delta)
+{
+    Game.player.x += Game.player.vx * delta;
+    Game.player.y += Game.player.vy * delta;
+
+    Game.Forces.forEach((force, i) => {
+        force.center.x += Game.Motion[i].x * delta;
+        force.center.y += Game.Motion[i].y * delta;
+
+        if(force.center.x < 0)
+        {
+            force.center.x += Game.width;
+        }
+        else if(force.center.x > Game.width)
+        {
+            force.center.x -= Game.width;
+        }
+
+        if(force.center.y < 0)
+        {
+            force.center.y += Game.height;
+        }
+        else if(force.center.y > Game.height)
+        {
+            force.center.y -= Game.height;
+        }
+    })//*/
+
+    ParticleDynamics.UpdateDebug(delta);
+}
+
+function InterfaceUpdate(delta)
+{
+    document.getElementById("renderfps").innerText = "Rendering: " + Math.round(Game.PIXIApp.ticker.FPS).toString() + "fps";
+    document.getElementById("simulationfps").innerText = "Simulation: " + Math.round(Game.simulationTPS).toString() + "fps";
+}
+
+function SetSimulationTPS(tps)
+{
+    if(Game.simulationInterval !== undefined)
+    {
+        clearInterval(Game.simulationInterval);
+    }
+    let millis = 1000 / tps;
+    Game.simulationInterval = setInterval(Tick, millis);
+    Game.simulationTPS = tps;
+}
+
+// https://github.com/kittykatattack/learningPixi#introduction
+function SetupKey(value) {
+    let key = {};
+    key.value = value;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    // downHandler
+    key.downHandler = (event) => {
+        if(event.key === key.value) {
+            if(key.isUp && key.press) {
+                key.press();
+            }
+            key.isDown = true;
+            key.isUp = false;
+            event.preventDefault();
+        }
+    };
+    // upHandler
+    key.upHandler = (event) => {
+        if(event.key === key.value) {
+            if(key.isDown && key.release) {
+                key.release();
+            }
+            key.isDown = false;
+            key.isUp = true;
+            event.preventDefault();
+        }
+    };
+    // attach event listeners
+    const downListener = key.downHandler.bind(key);
+    const upListener = key.upHandler.bind(key);
+    window.addEventListener("keydown", downListener, false);
+    window.addEventListener("keyup", upListener, false);
+    // detach event listeners
+    key.unsubscribe = () => {
+        window.removeEventListener("keydown", downListener);
+        window.removeEventListener("keyup", upListener);
+    };
+
+    return key;
+}
